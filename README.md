64556f7f6a23baebb986f8306fe2b1a0385c46b6
gauzewang <gauzewang@gmail.com>
2017-08-13 11:43:01 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# H5-Christmas
-圣诞许愿活动H5（基于VUE重构）
+# christmas
+
+> a wechat project using vue
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/main/p6/P6.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="p6">
+
+    <top-layers :show_menu="true" :show_award_btn="false">
+      <cartoon robot="ledi" class="robot1"></cartoon>
+      <cartoon robot="huang" class="robot2"></cartoon>
+      <cartoon robot="mei" class="robot3"></cartoon>
+    </top-layers>
+
+    <div class="content">
+
+      <div class="dialog" v-if="award === '一等奖'">
+        <div class="msg"></div>
+        <div class="award"></div>
+        <div class="form">
+          <div class="input_wrap">
+            <input class="input" placeholder="请输入您的地址" id="addr" type="text"/>
+          </div>
+          <div class="input_wrap">
+            <input class="input" placeholder="请输入您的电话" id="tel" type="tel"/>
+          </div>
+          <btn btn_name="ci"></btn>
+        </div>
+      </div>
+
+      <div class="dialog" v-else-if="['二等奖', '三等奖', '四等奖', '五等奖'].includes(award)">
+        <div class="msg"></div>
+        <div class="msg2"></div>
+        <div class="luckybag"></div>
+        <div class="msg3"></div>
+      </div>
+
+      <div class="dialog" v-else>
+        <div class="msg4"></div>
+      </div>
+
+      <div class="award_rule">
+        <div class="word"></div>
+        <div class="detail_wrap">
+          <div class="btn_detail" @click="showGift = !showGift"></div>
+          <div class="award_detail" v-show="showGift">
+            <img src="./image/gift_detail.png"/>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import TopLayers from '../../base/top_layers/TopLayers'
+  import Cartoon from '../../base/cartoon/Cartoon'
+  import Btn from '../../base/Btn/Btn'
+
+  export default {
+    name: 'p6',
+    components: {
+      'top-layers': TopLayers,
+      'cartoon': Cartoon,
+      'btn': Btn
+    },
+    data () {
+      return {
+        award: this.$route.params.award,
+        showGift: false
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import "style/p6";
+</style>
